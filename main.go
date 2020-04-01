752c35109c3f47cbc3d8cfb77f369ea9bfcf0815
black-desk <814727823@qq.com>
2020-04-01 14:42:21 +0800

    change import url

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 	"runtime"
 	"syscall"
 
-	"github.com/zu1k/clashr/config"
-	C "github.com/zu1k/clashr/constant"
-	"github.com/zu1k/clashr/hub"
+	"github.com/black-desk/clashr/config"
+	C "github.com/black-desk/clashr/constant"
+	"github.com/black-desk/clashr/hub"
 
 	log "github.com/sirupsen/logrus"
 )
